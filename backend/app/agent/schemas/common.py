874b05e674129aee0324884d5cce821ac5005ee4
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2025-05-26 23:49:32 +0530

    coordinates schema updated

--- backend/app/agent/schemas/common.py
+++ backend/app/agent/schemas/common.py
@@ -1,5 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Coordinates(BaseModel):
-    latitude: float
-    longitude: float
+    latitude: float = Field(..., alias="lat")
+    longitude: float = Field(..., alias="lng")
+
+    class Config:
+        allow_population_by_field_name = True
