eb2fa5b3af39781c0d6ae555fe4febc0096ef966
Janeesha Wickramasinghe <wickramasinghejj.21@uom.lk>
2025-05-29 13:59:01 +0530

    btn style changed

--- frontend/app/admin/disasters/page.tsx
+++ frontend/app/admin/disasters/page.tsx
@@ -196,7 +196,7 @@ export default function DisasterPage() {
 
   return (
     <div className="fixed inset-0 py-3 md:left-64 md:right-0 overflow-auto px-4 md:px-6">
-      <header className="mb-6 ml-8 md:ml-0 flex justify-between items-center">
+      <header className="mb-6 ml-8 md:ml-0 flex flex-wrap justify-between items-center">
         <h1 className="text-2xl md:text-3xl font-bold text-slate-800 dark:text-slate-200">Disasters</h1>
 
         {/* <div className="flex justify-end"> */}
